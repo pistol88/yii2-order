0436f97f2fcf12cbbd0f000c843aa7b5651ed570
Ilya <php@php.name>
2016-09-16 15:07:13 +0500

    choose options

--- views/tools/products_list.php
+++ views/tools/products_list.php
@@ -3,6 +3,7 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 use pistol88\cart\widgets\BuyButton;
 use pistol88\cart\widgets\ChangeCount;
+use pistol88\cart\widgets\ChangeOptions;
 $this->registerJs("$(document).on('renderCart', function() {
         window.parent.pistol88.createorder.updateCart();
     });");
@@ -12,7 +13,22 @@ $this->registerJs("$(document).on('renderCart', function() {
 .to-order {
     padding: 2px;
     margin-top: 2px;
-}    
+}
+.pistol88-order-prodlist-widgets {
+    font-size: 12px;
+}
+
+.pistol88-change-count, .pistol88-cart-buy-button {
+    display: inline;
+    width: 45%;
+}
+
+.products-list select {
+    padding: 1px;
+    margin: 1px;
+    font-size: 12px;
+    height: auto;
+}
 </style>
 <div class="products-list">
 
@@ -21,7 +37,29 @@ $this->registerJs("$(document).on('renderCart', function() {
         'filterModel' => $searchModel,
         'columns' => [
             ['attribute' => 'id', 'options' => ['style' => 'width: 45px;']],
-            'name',
+            [
+                'attribute' => 'name',
+                'content' => function($product) {
+                    $name = $product->name;
+
+                    $count = ChangeCount::widget(['model' => $product->sellModel]);
+                    
+                    if($product->getCartOptions()) {
+                        $options = ChangeOptions::widget(['model' => $product]);
+                    } else {
+                        $options = null;
+                    }
+                    
+                    $btn = BuyButton::widget([
+                        'model' => $product->sellModel,
+                        'text' => yii::t('order', 'To order'),
+                        'htmlTag' => 'a',
+                        'cssClass' => 'btn btn-success to-order'
+                    ]);
+                    
+                    return Html::tag('div', $name.$options.$count.$btn, ['class' => 'pistol88-order-prodlist-widgets']);
+                }
+            ],
             'code',
 			[
 				'attribute' => 'category_id',
@@ -40,19 +78,6 @@ $this->registerJs("$(document).on('renderCart', function() {
 			[
 				'label' => yii::t('order', 'Price'),
 				'value' => 'price',
-                'content' => function($product) {
-                    $price = $product->price;
-
-                    $count = ChangeCount::widget(['model' => $product->sellModel]);
-                    $btn = BuyButton::widget([
-                        'model' => $product->sellModel,
-                        'text' => yii::t('order', 'To order'),
-                        'htmlTag' => 'a',
-                        'cssClass' => 'btn btn-success to-order'
-                    ]);
-                    
-                    return $product->sellModel->getCartPrice().$count.$btn;
-                }
 			],
         ],
     ]); ?>
