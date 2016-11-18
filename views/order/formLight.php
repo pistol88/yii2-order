ae8e0022d7e5ff3943aa807b0b7f395ae2e67b0d
Trishkin Vadim <halumein@gmail.com>
2016-11-18 15:35:58 +0500

    fix

--- views/order/formLight.php
+++ views/order/formLight.php
@@ -1,9 +1,15 @@
 <div class="custom-order-form-light-container" style="max-width: 320px;">
     <?php if (Yii::$app->service->splitOrderPerfome) {
-        $staffers = isset(yii::$app->worksess->soon()->users) ? yii::$app->worksess->soon()->users : null;
-    } else {
-        $staffers = null;
-    } ?>
+            if (isset(yii::$app->worksess->soon()->users)) {
+                $staffers = yii::$app->worksess->soon()->getUsers();
+                $staffers = $staffers->all();
+            } else {
+                $staffers = null;
+            }
+        } else {
+            $staffers = null;
+        }
+    ?>
 
     <?= \pistol88\order\widgets\OrderFormLight::widget([
         'useAjax' => $useAjax,
