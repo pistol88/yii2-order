be55c10179a71d1f5395baf1a372a5373747ef4c
Trishkin Vadim <halumein@gmail.com>
2016-12-22 18:14:34 +0500

    create_order event name

--- Module.php
+++ Module.php
@@ -7,9 +7,9 @@ use yii;
 class Module extends \yii\base\Module
 {
     const EVENT_ORDER_CREATE = 'create';
-    const EVENT_ORDER_DELETE = 'delete_order';
+    const EVENT_ORDER_DELETE = 'delete';
     const EVENT_ELEMENT_DELETE = 'delete_element';
-    
+
     public $orderStatuses = ['new' => 'Новый', 'approve' => 'Подтвержден', 'cancel' => 'Отменен', 'process' => 'В обработке', 'done' => 'Выполнен'];
     public $defaultStatus = 'new';
     public $successUrl = '/order/info/thanks/';
