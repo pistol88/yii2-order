129f6865f05ed63ca3b4733158ee5481588fb2cf
Trishkin Vadim <halumein@gmail.com>
2017-01-24 13:02:49 +0500

    Найти покупателя в заголовок панельки

--- widgets/views/choose_client.php
+++ widgets/views/choose_client.php
@@ -5,10 +5,10 @@ use yii\helpers\Html;
 <div class="choose-client row">
     <div class="col-md-12">
         <label class="control-label" for="order-user_id"><?=Yii::t('order', 'Buyer');?></label>
-        
+
 		<div class="row">
 			<div class="col-md-4"><?= Html::input('text', 'Order[user_id]', '', ['id' => 'order-user_id', 'class' => 'form-control', 'data-info-service' => Url::toRoute(['/order/tools/user-info'])]) ?></div>
-			<div class="col-md-8"><p><?=Html::a('<i class="glyphicon glyphicon-search"></i> Найти покупателя', '#usersModal', ['id' => 'choose-user-id', 'data-toggle' => "modal", 'data-target' => "#usersModal"]);?></a></p></div>
+			<div class="col-md-8"><p><?=Html::a('<i class="glyphicon glyphicon-search"></i> Найти покупателя', '#usersModal', ['id' => 'choose-user-id', 'data-toggle' => "modal", 'data-target' => "#usersModal"]);?></p></div>
 		</div>
 
         <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
@@ -17,4 +17,3 @@ use yii\helpers\Html;
         <?= $form->field($model, 'client_name')->textInput(['maxlength' => true]) ?>
     </div>
 </div>
-
