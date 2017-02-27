372b8c2df4834417cee30cc50a7b665103cf16dc
Ilya <pistol88@gmail.com>
2017-02-27 16:22:56 +0500

    formatting

--- web/js/orders-list.js
+++ web/js/orders-list.js
@@ -3,34 +3,34 @@ if (typeof pistol88 == "undefined" || !pistol88) {
 }
 
 pistol88.orders_list = {
-	elementsUrl: null,
+    elementsUrl: null,
     init: function() {
         $('.order-index .show-details').on('click', function(e) {
             var self = this;
             var thisTableRow = $(self).closest('tr');
-			//alert($(e.target.tagName).parent().html());
-			// if($(e.target.tagName).parent().get(0).tagName == 'A' | e.target.tagName == 'A') {
-			// 	return null;
-			// }
-			if(thisTableRow.next('tr').hasClass('order-detail')) {
-				thisTableRow.next('tr').remove();
-			}
-			else {
-				var id = thisTableRow.data('key');
+            //alert($(e.target.tagName).parent().html());
+            // if($(e.target.tagName).parent().get(0).tagName == 'A' | e.target.tagName == 'A') {
+            //     return null;
+            // }
+            if(thisTableRow.next('tr').hasClass('order-detail')) {
+                thisTableRow.next('tr').remove();
+            }
+            else {
+                var id = thisTableRow.data('key');
 
-				if(id) {
-					$(tr).find('td').css('opacity', '0.3');
+                if(id) {
+                    $(tr).find('td').css('opacity', '0.3');
 
-					$.post(pistol88.orders_list.elementsUrl, {ajax: true, orderId: id},
-						function(json) {
-							$(tr).after('<tr class="order-detail"><td colspan="100">'+json.elementsHtml+'</td></tr>');
-							$(tr).find('td').css('opacity', '1');
-						}, "json");
-				}
+                    $.post(pistol88.orders_list.elementsUrl, {ajax: true, orderId: id},
+                        function(json) {
+                            $(tr).after('<tr class="order-detail"><td colspan="100">'+json.elementsHtml+'</td></tr>');
+                            $(tr).find('td').css('opacity', '1');
+                        }, "json");
+                }
 
-				var tr = $(thisTableRow);
-			}
-		});
+                var tr = $(thisTableRow);
+            }
+        });
     },
 };
 
